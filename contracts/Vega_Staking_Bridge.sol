dca913216436d9eb3753b9a8ef6132505084cae1
danny <hollandcodeandgraphics@gmail.com>
2021-06-09 11:05:32 -0700

    created IStake interface and got contracts to compile

--- contracts/Vega_Staking_Bridge.sol
+++ contracts/Vega_Staking_Bridge.sol
@@ -3,20 +3,17 @@
 pragma solidity 0.8.1;
 
 import "./IERC20.sol";
+import "./IStake.sol";
 
 /// @title ERC20 Staking Bridge
 /// @author Vega Protocol
 /// @notice This contract manages the vesting of the Vega V2 ERC20 token
-contract ERC20_Staking_Bridge {
+contract Vega_Staking_Bridge is IStake {
   address public staking_token;
 
   constructor(address token) {
     staking_token = token;
   }
-
-  event Stake_Deposited(address indexed user, uint256 amount, bytes32 vega_public_key);
-  event Stake_Removed(address indexed user, uint256 amount, bytes32 vega_public_key);
-  event Stake_Transfered(address indexed from, address indexed to);
 
   /// @dev user => amount staked
   mapping(address => mapping(bytes32 => uint256)) public stake;
@@ -35,6 +32,7 @@ contract ERC20_Staking_Bridge {
   /// @notice This removes specified amount of stake if available to user
   /// @dev Emits Stake_Removed event if successful
   /// @param amount Amount of tokens to remove from staking
+  /// @param vega_public_key Target Vega public key from which to deduct stake
   function Remove_Stake(uint256 amount, bytes32 vega_public_key) public {
     stake[msg.sender][vega_public_key] -= amount;
     require(IERC20(staking_token).transfer(msg.sender, amount));
@@ -43,11 +41,13 @@ contract ERC20_Staking_Bridge {
 
   /// @notice This transfers all stake from the sender's address to the "new_address"
   /// @dev Emits Stake_Transfered event if successful
+  /// @param amount Stake amount to transfer
   /// @param new_address Target ETH address to recieve the stake
-  function Transfer_Stake(address new_address) public {
-    stake[msg.sender] -= amount;
-    stake[new_address] += amount;
-    emit Stake_Transfered(msg.sender, new_address);
+  /// @param vega_public_key Target Vega public key to be credited with the transfer
+  function Transfer_Stake(uint256 amount, address new_address, bytes32 vega_public_key) public {
+    stake[msg.sender][vega_public_key] -= amount;
+    stake[new_address][vega_public_key] += amount;
+    emit Stake_Transfered(msg.sender, new_address, vega_public_key);
   }
 }
 
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
 const Migrations = artifacts.require("Migrations");
+const Vega_Staking_Bridge = artifacts.require("Vega_Staking_Bridge");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+    deployer.deploy(Vega_Staking_Bridge);
+
 };
