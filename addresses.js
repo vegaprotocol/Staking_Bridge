da81157d72c957f7a4fd0473e16b875088f9c7de
danny <hollandcodeandgraphics@gmail.com>
2021-06-11 17:34:49 -0700

    address update

--- addresses.js
+++ addresses.js
@@ -1,4 +1,4 @@
 let addresses = {
-  staking_bridge_address: "0xdCAe292039cF02014e6470a50CbB590Af0FD0c4C",
+  staking_bridge_address: "0x0C2D9B54711B4531e26A2b1d2186C542F585839A",
   token_address: "0x7e503d51E18bF3d5825682A54B363799D4a8e344"
 }
