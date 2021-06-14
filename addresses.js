4bb1f07a57a696e5b2bb8b0ed927bae696ed6df0
danny <hollandcodeandgraphics@gmail.com>
2021-06-14 10:52:11 -0700

    ropsten redeploy

--- addresses.js
+++ addresses.js
@@ -1,4 +1,4 @@
 let addresses = {
-  staking_bridge_address: "0xf68a9F57D04F49edb3ad55817efc6481085FD57C",
+  staking_bridge_address: "0xce28aA54d173bbfa88b9C2247575c435Bd69B748",
   token_address: "0x7e503d51E18bF3d5825682A54B363799D4a8e344"
 }
